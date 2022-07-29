32a57589ae1a4aac0917e3fbd45fcae2c15b7d46
Albert Ibragimov <82354553+focusnic2021@users.noreply.github.com>
2022-07-29 12:35:30 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-open_weather_token = "7b101b34b8831f1182cd3cc7e5497013"
-tg_bot_token = "5470464222:AAFtafOKVbH8xEIHopus0d0W9vdx5lSdxiE"
+open_weather_token = "enter here token on openweather.org site"
+tg_bot_token = "enter here token for your tg-bot"
